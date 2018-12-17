451c8fae615c4860158cc4357de7e965b70145d1
Unknown <akkoumis@ece.auth.gr>
2018-12-17 18:27:38 +0200

    Main -> Struct point and read from file #2

--- main.c
+++ main.c
@@ -12,38 +12,40 @@ struct point {
     float x, y;
 };
 
+typedef struct point point;
+
 int main(int argc, char **argv) {
 
     int processID, noProcesses;
-    int size, loop, master, groups; // Size = # of elems, partLength = length of partition in each process
+    int totalSize, perProcessSize, loop, master, groups; // Size = # of elems
 
     float *distances;
-    struct point *points;
-    size = 16;
+    point *points;
+    totalSize = 16;
 
     MPI_Init(&argc, &argv);    /* starts MPI */
     MPI_Comm_rank(MPI_COMM_WORLD, &processID);    /* get current process id */
     MPI_Comm_size(MPI_COMM_WORLD, &noProcesses);    /* get number of processes */
+    perProcessSize = totalSize / noProcesses;
 
     // Each process read simultaneously its data, from the file
     FILE *fp;
     char buff[255], buff2[255];
-    float coordinate, coordinate2;
-    long int offset = (((long int) size) / noProcesses) * 2 * processID; // *2 because x and y are on different lines
 
+    long int offset = ((long int) perProcessSize) * 2 * processID; // *2 because x and y are on different lines
     fp = fopen("cities.txt", "r");
-    //fseek(fp, offset, SEEK_CUR);
-
     for (long int i = 0; i < offset; ++i) {
-        fscanf(fp, "%s", buff);
+        fscanf(fp, "%s", buff); // Moving stream according to offset
     }
 
-    for (int i = 0; i < size / noProcesses; ++i) {
+    points = (point *) malloc(perProcessSize * sizeof(point));
+
+    for (int i = 0; i < totalSize / noProcesses; ++i) {
         fscanf(fp, "%s", buff);
-        coordinate = atof(buff);
+        points[i].x = atof(buff);
         fscanf(fp, "%s", buff2);
-        coordinate2 = atof(buff2);
-        printf("%d:\t%f\t%f\n", offset / 2 + i + 1, coordinate, coordinate2);
+        points[i].y = atof(buff2);
+        printf("%ld:\t%f\t%f\n", offset / 2 + i + 1, points[i].x, points[i].y);
     }
 
     fclose(fp);
@@ -53,7 +55,7 @@ int main(int argc, char **argv) {
         groups = (int) pow(2, loop);
         //printf("Groups = %d\n", groups);
         noProcesses = noProcesses / groups; // No of processes per group
-        size = size / groups; // Size of the array for each group
+        totalSize = totalSize / groups; // Size of the array for each group
         master = processID / noProcesses; // Integer division <=> Floor, for positives
         master = master * groups;// Calculate the master of the process
         //printf("My Master is = %d\n", master);
@@ -61,8 +63,8 @@ int main(int argc, char **argv) {
         MPI_Comm communicator[1];
         MPI_Comm_split(MPI_COMM_WORLD, master, 0, communicator);
 
-        distances = (float *) malloc((size / noProcesses) * sizeof(float));
-        generateNumbers(distances, size / noProcesses, processID);
+        distances = (float *) malloc((totalSize / noProcesses) * sizeof(float));
+        generateNumbers(distances, totalSize / noProcesses, processID);
 
         //float median = mpiFindMedian(processID, noProcesses, size, distances, communicator);
     }
